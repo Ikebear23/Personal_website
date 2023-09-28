7756e6d7946e0a411b474b7fa6010bf4be24fe7d
Ikebear23 <117481660+Ikebear23@users.noreply.github.com>
2023-09-27 20:30:56 -0500

    Add files via upload

---
+++ careersim1/careersim1/index.js
@@ -0,0 +1,14 @@
+const indexhtml = ` <h1>Welcome to my page</h1>`;
+const container = document.createElement("div");
+container.innerHTML = indexhtml;
+document.getElementById("home").appendChild(container);
+/*
+const abouthtml = ` <h1>About me</h1>`;
+const container2 = document.createElement("div2");
+container2.innerHTML = abouthtml;
+document.getElementById("about").appendChild(container2);
+
+const portfoliohtml = ` <h1>My work</h1>`;
+const container3 = document.createElement("div3");
+container3.innerHTML = portfoliohtml;
+document.getElementById("portfolio").appendChild(container3); */
